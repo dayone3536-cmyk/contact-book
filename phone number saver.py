fc3e71a42bf2e09c1276e4e1b7aba408a18c2e07
dayone3536-cmyk <dayone3536@gmail.com>
2026-02-14 14:54:45 +0530

    Add files via upload

---
+++ phone number saver.py
@@ -0,0 +1,25 @@
+
+print("---WELCOME---")
+try:
+    def main():
+        user_input = str(int(input("enter your phone number: ")))
+        user_input_name = input("enter the owners name: ")
+        save = input("do you want to be save this number with this name??(y/n): ").lower()
+
+        if save == 'y':
+            with open("text.txt" , "a") as file:
+                file.write (user_input_name + ":" + user_input + "\n")
+
+
+                print(f"SAVED  {user_input_name} has {user_input} number ")
+                contact_search = input("do you want to search any number in your contacts?? (y/n): ").lower()
+                if contact_search == 'y':
+                    with open("text.txt" , "r") as data:
+                        contact = data.read()
+                        contact_name = input("ENTER THE NAME FOR SEARCHING: ")
+                        if contact_name in contact:
+                            print(f'FOUND!! it is {contact}')
+                        else: print("NOT FOUND!!")
+    main()
+except ValueError:
+    print("INVALID!!")
